654d1e9f2775d848e4fb4f878278c83e132e1061
Anzor Karmov <a.karmov@lipt-soft.ru>
2026-03-18 22:13:55 +0300

    init: add UniqueElements task

---
+++ tasks/arrays/UniqueElements.java
@@ -0,0 +1,16 @@
+package tasks.arrays;
+
+public class UniqueElements {
+
+    /**
+     * Возвращает новый массив с уникальными элементами в том же порядке.
+     *
+     * Пример:
+     *   вход:  [1, 2, 2, 3, 1]
+     *   выход: [1, 2, 3]
+     */
+    public static int[] getUnique(int[] input) {
+        // TODO: реализовать
+        return new int[]{};
+    }
+}
