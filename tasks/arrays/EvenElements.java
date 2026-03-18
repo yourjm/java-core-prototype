0262f9053b755447d5b4e6509ba2dad177ba3cb3
Anzor Karmov <a.karmov@lipt-soft.ru>
2026-03-18 23:32:32 +0300

    new task

---
+++ tasks/arrays/EvenElements.java
@@ -0,0 +1,16 @@
+package tasks.arrays;
+
+public class EvenElements {
+
+    /**
+     * Возвращает новый массив содержащий только чётные элементы.
+     *
+     * Пример:
+     *   вход:  [1, 2, 3, 4, 5, 6]
+     *   выход: [2, 4, 6]
+     */
+    public static int[] getEven(int[] input) {
+        // TODO: реализовать
+        return new int[]{};
+    }
+}
